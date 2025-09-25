f484e37447b188bfafcaf71dadf71d54f16910ff
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-09-25 23:41:08 +0200

    feat: enhance Input component with improved value handling and validation logic

--- app/components/FormBuilder/FormField/Input.vue
+++ app/components/FormBuilder/FormField/Input.vue
@@ -1,24 +1,47 @@
 <script setup lang="ts" generic="
 Context extends FormContext,
 Keys extends FormContextKeys<Context>,
-InputPropsGeneric = unknown
+InputPropsGeneric = unknown,
+ModelValue = unknown
 "
 >
 import type { FieldPropSet, FormContext, FormContextKeys } from "../types";
+import * as _ from "lodash-es";
+
 import { FormFieldInputs } from "../types";
 
-defineProps<
+const { ctx, path } = defineProps<
   FieldPropSet<InputPropsGeneric> & {
     ctx: Context
     path: Keys
   }
 >();
+
+function onValueChange(newValue: ModelValue) {
+  const validator = getValidatorByPath(ctx.schema, path);
+
+  if (validator) {
+    if (validator.safeParse(null).success && newValue === "") {
+      _.set(ctx.form, path, null);
+    }
+    else if (validator.safeParse(undefined).success && newValue === "") {
+      _.set(ctx.form, path, undefined);
+    }
+    else {
+      _.set(ctx.form, path, newValue);
+    }
+  }
+  else {
+    _.set(ctx.form, path, newValue);
+  }
+}
 </script>
 
 <template>
   <component
     :is="FormFieldInputs[type]"
     v-bind="props"
-    v-model="ctx.form[path]"
+    :model-value="_.get(ctx.form, path)"
+    @update:model-value="onValueChange"
   />
 </template>
