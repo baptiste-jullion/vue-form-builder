0f95cc0730c4d535eac41f703741f3b5ac6a21c8
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-09-26 11:49:54 +0200

    feat: update FormFieldInputs and FieldPropSet to include Checkbox and Number types with proper props and emits

--- app/components/FormBuilder/types.ts
+++ app/components/FormBuilder/types.ts
@@ -1,8 +1,12 @@
 import type { z, ZodType } from "zod";
+import type { CheckboxEmits, CheckboxProps } from "~/components/Inputs/Checkbox.vue";
 import type { DateProps } from "~/components/Inputs/Date.vue";
-import type { SelectProps } from "~/components/Inputs/Select.vue";
-import type { TextProps } from "~/components/Inputs/Text.vue";
+import type { NumberProps } from "~/components/Inputs/Number.vue";
+import type { SelectEmits, SelectProps } from "~/components/Inputs/Select.vue";
+import type { TextEmits, TextProps } from "~/components/Inputs/Text.vue";
+import Checkbox from "~/components/Inputs/Checkbox.vue";
 import Date from "~/components/Inputs/Date.vue";
+import Number from "~/components/Inputs/Number.vue";
 import Select from "~/components/Inputs/Select.vue";
 import Text from "~/components/Inputs/Text.vue";
 
@@ -10,14 +14,19 @@ export const FormFieldInputs = {
   Date,
   Select,
   Text,
+  Number,
+  Checkbox,
 } as const;
 
 export type FormFieldInputsKeys = keyof typeof FormFieldInputs;
 
-export type FieldPropSet<I = unknown>
-  = | { type: "Select", props?: SelectProps<I> }
-    | { type: "Text", props?: TextProps }
-    | { type: "Date", props?: DateProps };
+export type FieldPropSet<I>
+  = | { type: "Select", props?: SelectProps<I>, emits?: Partial<EmitsToOptions<SelectEmits>> }
+    | { type: "Text", props?: TextProps, emits?: Partial<EmitsToOptions<TextEmits>> }
+    | { type: "Date", props?: DateProps, emits?: object }
+    | { type: "Number", props?: NumberProps, emits?: object }
+    | { type: "Checkbox", props?: CheckboxProps, emits?: Partial<EmitsToOptions<CheckboxEmits>> }
+    ;
 
 export interface FormContext { schema?: ZodType<object>, form: Record<string, string> }
 
