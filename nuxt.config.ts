12e3c15d25263615e6fe04fdc4e5f891b62136f0
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-11-07 14:25:01 +0100

    feat: add app head configuration with external stylesheet link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   modules: ["@nuxt/eslint"],
   devtools: { enabled: true },
   compatibilityDate: "2025-07-15",
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/gh/nandaleio/shadcn-classless@main/dist/shadcn-classless.css",
+        },
+      ],
+    },
+  },
 });
