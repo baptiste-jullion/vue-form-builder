5deb28b42e7f21ec52dcb5cce700807bf0735b45
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-11-07 15:15:13 +0100

    feat: enhance i18n support in Field.vue with localized rendering for multiple languages

--- app/components/FormBuilder/Field.vue
+++ app/components/FormBuilder/Field.vue
@@ -9,13 +9,30 @@ import type { FieldPropSet, FormContext, FormContextKeys } from "~/components/Fo
 const props = defineProps<FieldPropSet<InputPropsGeneric> & {
   ctx: Context
   path: Keys
+  dependencies?: Exclude<FormContextKeys<Context>, Keys>[]
+  localized?: boolean
 }>();
 </script>
 
+<!-- Add better i18n support, temporarily using hardcoded locales should also include localization in Label, Input and Errors components -->
+
 <template>
-  <FormBuilderFieldLabel :ctx :path />
-  <FormBuilderFieldInput
-    v-bind="{ ...props }"
-  />
-  <FormBuilderFieldErrors :ctx :path />
+  <div>
+    <template v-if="localized">
+      <div v-for="locale in ['fr', 'en']" :key="locale">
+        <FormBuilderFieldLabel :ctx :path="(`${path}.${locale}` as Keys)" />
+        <FormBuilderFieldInput
+          v-bind="{ ...props, path: (`${path}.${locale}` as Keys) }"
+        />
+        <FormBuilderFieldErrors :ctx :path="(`${path}.${locale}` as Keys)" />
+      </div>
+    </template>
+    <template v-else>
+      <FormBuilderFieldLabel :ctx :path />
+      <FormBuilderFieldInput
+        v-bind="{ ...props }"
+      />
+      <FormBuilderFieldErrors :ctx :path />
+    </template>
+  </div>
 </template>
